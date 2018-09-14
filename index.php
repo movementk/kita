86dd5ba7fad2ecb7f95b24c31f299ede815d93e7
kimyh5392 <mk.kimyh@gmail.com>
2018-09-14 12:14:17 +0900

    메인 퍼블리싱 작업중 . . . .

---
+++ index.php
@@ -0,0 +1,394 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>한국무역협회</title>
+        <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/fullpage/fullpage.css">
+        <link rel="stylesheet" href="/assets/fontello/css/fontello.css">
+        <link rel="stylesheet" href="/assets/css/default.css">
+        <!--[if IE 9]>
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie9.min.css" rel="stylesheet">
+        <![endif]-->
+        <!--[if lte IE 8]>
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie8.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>
+        <![endif]-->
+    </head>
+    <body>
+        
+        <!-- Header -->
+        <header id="header">
+            <h1 class="logo">
+                <a href="/"><img src="/assets/images/logo.png" alt="logo"></a>
+            </h1>
+            <button type="button" class="btn btn-nav">
+                <span class="sr-only">메뉴열기</span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+        </header>
+        <!-- Nav -->
+        <nav id="nav">
+            <div class="backdrop"></div>
+            <div class="nav-body">
+                <button type="button" class="btn nav-close">
+                    <img src="/assets/images/ico_btn_close.png" alt="닫기">
+                    <span class="sr-only">메뉴닫기</span>
+                </button>
+                <div class="gnb-menu">
+                    <ul>
+                        <li>
+                            <a href="#">전시장 위치 안내<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">服务贸易展区<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">服务贸易<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">贸 智易能展及区高端装备展区<span></span></a>
+                            <ul>
+                                <li><a href="#">3H</a></li>
+                                <li><a href="#">4.1H</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">消费及电子家电展区•<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">服装服饰及日用消费品展区<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">医疗器械及医药保健展区<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">食品及农产品展区<span></span></a>
+                            <ul>
+                                <li><a href="#">menu1</a></li>
+                                <li><a href="#">menu2</a></li>
+                                <li><a href="#">menu3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        
+        
+        
+        
+        <!-- main Content Start -->
+        <main id="fullpage">
+            <!-- intro -->
+            <section class="section intro">
+                <div class="container">
+                    <h1>
+                        2018 CHINA<br>INTERNATIONAL<br>IMPORT EXPO
+                    </h1>
+                    <a href="javascript:;" class="moveBtn" data-type="eng">ENG</a>
+                    <a href="javascript:;" class="moveBtn" data-type="chn">CHN</a>
+                </div>
+            </section>
+            
+            <!-- location-guide -->
+            <section class="section location-guide">
+                <div class="container">
+                    <div class="section-header">
+                        <h2>전시장 위치 안내</h2>
+                    </div>
+                    <div class="section-content">
+                        <ol class="guide-list">
+                            <li>
+                                <a href="#">
+                                    <h3>1H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>2H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>3H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>4.1H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>5.1H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>6.1H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>7.1H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>7.2H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <h3>8.2H</h3>
+                                    <p class="summary">服务贸易展区</p>
+                                </a>
+                            </li>
+                        </ol>
+                        <article class="location-area">
+                            <div class="map">
+                                <a href="#" class="area-1">
+                                    <img src="/assets/images/main/img_area01.png" alt="">
+                                </a>
+                                <a href="#" class="area-2">
+                                    <img src="/assets/images/main/img_area02.png" alt="">
+                                </a>
+                                <a href="#" class="area-3">
+                                    <img src="/assets/images/main/img_area03.png" alt="">
+                                </a>
+                                <a href="#" class="area-4">
+                                    <img src="/assets/images/main/img_area04.png" alt="">
+                                </a>
+                                <a href="#" class="area-5">
+                                    <img src="/assets/images/main/img_area05.png" alt="">
+                                </a>
+                                <a href="#" class="area-6">
+                                    <img src="/assets/images/main/img_area06.png" alt="">
+                                </a>
+                                <a href="#" class="area-7">
+                                    <img src="/assets/images/main/img_area07.png" alt="">
+                                </a>
+                                <a href="#" class="area-8">
+                                    <img src="/assets/images/main/img_area08.png" alt="">
+                                </a>
+                                <a href="#" class="area-9">
+                                    <img src="/assets/images/main/img_area09.png" alt="">
+                                </a>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </section>
+            
+            <!-- information -->
+            <section class="section service-trade">
+                <div class="container">
+                    <div class="section-header">
+                        <h2>服务贸易展区<small>(1A7-024)</small></h2>
+                    </div>
+                    <div class="section-content">
+                        <nav class="trade-tab">
+                            <ul>
+                                <li class="active"><a href="#">服务贸易展区</a></li>
+                                <li><a href="#">服务贸易</a></li>
+                                <li><a href="#">贸 智易能展及区高端装备展区</a></li>
+                                <li><a href="#">消费及电子家电展区•</a></li>
+                                <li><a href="#">服装服饰及日用消费品展区</a></li>
+                                <li><a href="#">医疗器械及医药保健展区</a></li>
+                                <li><a href="#">食品及农产品展区</a></li>
+                            </ul>
+                        </nav>
+                        <article class="trade-list">
+                            <ul class="row">
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo01.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo02.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo03.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo04.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo05.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo06.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo07.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo08.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo09.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo10.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo11.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                                <li class="col-3">
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_logo12.jpg" class="img-fluid" alt="">
+                                        </figure>
+                                    </a>
+                                </li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+                <footer id="footer">
+                    <div class="container">
+                        <p class="copyright">COPYRIGHT (C) 2018 KITA. ALL RUGHTS RESERVED.</p>
+                    </div>
+                </footer>
+            </section>
+        </main>
+        
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/assets/fullpage/vendors/easings.min.js"></script>
+        <script src="/assets/fullpage/vendors/scrolloverflow.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.5/jquery.fullpage.js"></script>
+        <script>
+            (function($){
+                // full page 사용
+                $(document).ready(function(){
+                    $('#fullpage').fullpage({
+                        autoScrolling: true,
+                        scrollHorizontally: true
+                    });
+                });
+                
+                // nav event 
+                $(document).on('click', '#header > .btn-nav', function(){
+                    $('body').addClass('opened');
+                });
+                $(document).on('click', '#nav > .backdrop, #nav .nav-close', function(){
+                    $('body').removeClass('opened');
+                });
+                
+                // gnb menu event
+                $(document).on('click', '.gnb-menu > ul > li > a', function() {
+                    if ($(this).parent().hasClass('active')) {
+                        $(this).parent().removeClass('active');
+                    } else {
+                        $(this).parent().siblings('.active').removeClass('active');
+                        $(this).parent().addClass('active');
+                    }
+                });
+                
+                // location-guide event
+                $(document).on('mouseenter focus', '.guide-list > li > a', function() {
+                    $(this).parent().siblings().removeClass('active');
+                    $(this).parent().addClass('active');
+                });
+                $(document).on('mouseleave', '.guide-list', function() {
+                    $('.guide-list > li').removeClass('active');
+                });
+                
+                $(document).on("click", ".moveBtn", function(){
+                    var lang = $(this).attr("data-type");
+                    var event = $.Event("mousewheel");
+                    event.deltaY = -1;
+                    $("body").trigger("event");
+                })
+            })(jQuery);
+        </script>
+    </body>
+</html>
